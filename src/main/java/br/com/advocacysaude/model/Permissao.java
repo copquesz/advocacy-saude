4e150a1b495a89258e478fc585cb7eb1595b77b6
Lucas Copque <copque.faculdade@gmail.com>
2019-03-12 16:36:44 -0300

    model de permissao

---
+++ src/main/java/br/com/advocacysaude/model/Permissao.java
@@ -0,0 +1,45 @@
+package br.com.advocacysaude.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import br.com.advocacysaude.enumerated.TipoPermissao;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Permissao implements Serializable{
+	
+	private static final long serialVersionUID = -412509312842452004L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro;
+	
+	private UsuarioEntidade entidade;
+	
+	private UsuarioComum usuario;
+	
+	private TipoPermissao status;
+	
+	public Permissao() {
+		super();
+		this.dataCadastro = new Date();
+		this.status = TipoPermissao.ACESSO_BLOQUEADO;
+	}
+
+}
