c3b85a441288827af6ed9e38e97059a66840dac1
Lucas Copque <copque.faculdade@gmail.com>
2019-03-12 15:39:32 -0300

    model de curso

---
+++ src/main/java/br/com/advocacysaude/model/Curso.java
@@ -0,0 +1,68 @@
+package br.com.advocacysaude.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Curso implements Serializable{
+
+	private static final long serialVersionUID = -3247927894882336687L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro;
+	
+	private String titulo;
+	
+	@Column(columnDefinition = "TEXT")
+	private String introducao;
+	
+	@Column(columnDefinition = "TEXT")
+	private String conteudo;
+	
+	@Column(columnDefinition = "TEXT")
+	private String link;
+	
+	@JoinColumn(name = "banner_id")
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Arquivo banner;
+	
+	@JoinColumn(name = "usuario_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Usuario usuario;
+	
+	public Curso() {
+		this.dataCadastro = new Date();
+
+		this.titulo = "Não Informado";
+		this.introducao = "Não Informado";
+		this.conteudo = "Não Informado";
+		this.link = "";
+		this.banner = null;
+		this.usuario = null;
+	}
+
+}
