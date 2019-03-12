7e394912e473a525205866ec72f921ed1813a46b
Lucas Copque <copque.faculdade@gmail.com>
2019-03-12 17:10:18 -0300

    + tipo de rede social

--- src/main/java/br/com/advocacysaude/model/RedeSocial.java
+++ src/main/java/br/com/advocacysaude/model/RedeSocial.java
@@ -3,10 +3,13 @@ package br.com.advocacysaude.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import br.com.advocacysaude.enumerated.TipoRedeSocial;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,18 +18,19 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class RedeSocial implements Serializable{
-	
+public class RedeSocial implements Serializable {
+
 	private static final long serialVersionUID = -811446266240367775L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String link;
-	
+
 	private Long seguidores;
-	
-	
+
+	@Enumerated(EnumType.ORDINAL)
+	TipoRedeSocial tipo;
 
 }
