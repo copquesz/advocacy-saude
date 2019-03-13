40ccaa25f8fc137fd82fabfaacab738ee5d52a41
Lucas Copque <copque.faculdade@gmail.com>
2019-03-13 16:39:30 -0300

    repository de curso

---
+++ src/main/java/br/com/advocacysaude/repository/CursoRepository.java
@@ -0,0 +1,11 @@
+package br.com.advocacysaude.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.advocacysaude.model.Curso;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long>{
+
+}
