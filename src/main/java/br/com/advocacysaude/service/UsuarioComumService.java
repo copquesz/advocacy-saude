ffe08cc2a7bddd3fa8680e317d8cc7d8f796b77e
Lucas Copque <copque.faculdade@gmail.com>
2019-03-13 16:23:27 -0300

    service de usuario comum

---
+++ src/main/java/br/com/advocacysaude/service/UsuarioComumService.java
@@ -0,0 +1,28 @@
+package br.com.advocacysaude.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.advocacysaude.model.UsuarioComum;
+import br.com.advocacysaude.repository.UsuarioComumRepository;
+
+public class UsuarioComumService {
+	
+	@Autowired
+	private UsuarioComumRepository repository;
+	
+	public UsuarioComum save(UsuarioComum usuarioComum) {
+		return repository.save(usuarioComum);
+	}
+	
+	public Optional<UsuarioComum> findById(Long id) {
+		return repository.findById(id);
+	}
+	
+	public List<UsuarioComum> findAll(){
+		return repository.findAll();
+	}
+
+}
