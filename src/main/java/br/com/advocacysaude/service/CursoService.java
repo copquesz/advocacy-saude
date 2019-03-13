af6a27e8f8d71ab33bd8bb468b3bb458817c56a0
Lucas Copque <copque.faculdade@gmail.com>
2019-03-13 16:39:38 -0300

    service de curso

---
+++ src/main/java/br/com/advocacysaude/service/CursoService.java
@@ -0,0 +1,30 @@
+package br.com.advocacysaude.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.advocacysaude.model.Curso;
+import br.com.advocacysaude.repository.CursoRepository;
+
+@Service
+public class CursoService {
+	
+	@Autowired
+	private CursoRepository repository;
+
+	public Curso save(Curso curso) {
+		return repository.save(curso);
+	}
+	
+	public Optional<Curso> findById(Long id){
+		return repository.findById(id);
+	}
+	
+	public List<Curso> findAll(){
+		return repository.findAll();
+	}
+	
+}
