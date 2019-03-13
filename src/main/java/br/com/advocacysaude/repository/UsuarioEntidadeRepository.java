2c8ffef5ce305c9df8ee27b967baf8f2bdb9b884
Lucas Copque <copque.faculdade@gmail.com>
2019-03-13 16:32:36 -0300

    adicionando a anotação mvc

---
+++ src/main/java/br/com/advocacysaude/repository/UsuarioEntidadeRepository.java
@@ -0,0 +1,11 @@
+package br.com.advocacysaude.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.advocacysaude.model.UsuarioEntidade;
+
+@Repository
+public interface UsuarioEntidadeRepository extends JpaRepository<UsuarioEntidade, Long>{
+
+}
--- src/main/java/br/com/advocacysaude/service/UsuarioComumService.java
+++ src/main/java/br/com/advocacysaude/service/UsuarioComumService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.advocacysaude.model.UsuarioComum;
 import br.com.advocacysaude.repository.UsuarioComumRepository;
 
+@Service
 public class UsuarioComumService {
 	
 	@Autowired
