1d0f82aacc5743fba9a306b2014a2db5f6e03a7e
Lucas Copque <copque.faculdade@gmail.com>
2019-03-13 16:33:03 -0300

    adicionando a anotação de mvc

--- src/main/java/br/com/advocacysaude/repository/UsuarioComumRepository.java
+++ src/main/java/br/com/advocacysaude/repository/UsuarioComumRepository.java
@@ -1,9 +1,11 @@
 package br.com.advocacysaude.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.advocacysaude.model.UsuarioComum;
 
+@Repository
 public interface UsuarioComumRepository extends JpaRepository<UsuarioComum, Long> {
 
 }
