3e3b4ba49421adc8b723381cfdcc855e3c8d3d2a
Lucas Copque <copque.faculdade@gmail.com>
2019-03-14 17:45:37 -0300

    adicionando o metodo de busca por email

--- src/main/java/br/com/advocacysaude/repository/UsuarioRepository.java
+++ src/main/java/br/com/advocacysaude/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.advocacysaude.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import br.com.advocacysaude.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
+	Optional<Usuario> findByEmail(String email);
 	boolean existsByEmailAndSenha(String email, String senha);
 
 }
