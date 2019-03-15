8dfeaa794fee4e1afb8cefe633805dd9e51c54be
Lucas Copque <copque.faculdade@gmail.com>
2019-03-15 17:37:56 -0300

    ajustando metodo de login

--- src/main/java/br/com/advocacysaude/controller/UsuariosController.java
+++ src/main/java/br/com/advocacysaude/controller/UsuariosController.java
@@ -1,14 +1,16 @@
 package br.com.advocacysaude.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.com.advocacysaude.enumerated.TipoUsuario;
 import br.com.advocacysaude.model.Usuario;
 import br.com.advocacysaude.model.UsuarioComum;
 import br.com.advocacysaude.model.UsuarioEntidade;
@@ -26,47 +28,61 @@ public class UsuariosController {
 	@Autowired
 	public UsuariosController(UsuarioService us, UsuarioComumService ucs, UsuarioEntidadeService ues) {
 		super();
-		this.us= us;
+		this.us = us;
 		this.ucs = ucs;
 		this.ues = ues;
 	}
 
-	@GetMapping("/usuario/login")
-	public String getLogin(HttpServletRequest request, Model model) {
+	@GetMapping("/login")
+	public String getLogin(HttpServletRequest request, Model model,
+			@ModelAttribute("redirectAttributesLogin") String redirectAttributesLogin) {
 
 		// paths da requisição e de redirecionamento
 		model.addAttribute("path", request.getContextPath());
+
+		if (redirectAttributesLogin.equals("incorreto")) {
+			model.addAttribute("redirectAttributesLogin", true);
+		}
 
 		return "main/usuario/login";
 	}
 
-	public String postLogin(Usuario usuario, HttpServletRequest request, Model model) {
+	@PostMapping("/login")
+	public String postLogin(Usuario usuario, HttpServletRequest request, Model model, HttpSession session,
+			RedirectAttributes redirectAttributes) {
 
 		// paths da requisição e de redirecionamento
 		model.addAttribute("path", request.getContextPath());
-		
+
 		// Verifica se possui cadastro para email e senha informados.
-		if(us.existsByEmailAndSenha(usuario.getEmail(), usuario.getSenha())) {
-			
+		if (us.existsByEmailAndSenha(usuario.getEmail(), usuario.getSenha())) {
+
 			// Carrega os dados do usuário abstrato
 			usuario = us.findByEmail(usuario.getEmail()).get();
-			
+			session.setAttribute("usuario", usuario);
+
 			// Direciona para o painel de acordo com o tipo de usuário
 			switch (usuario.getTipo()) {
 			case USUARIO_PF:
 				UsuarioComum usuarioComum = new UsuarioComum();
 				usuarioComum = ucs.findById(usuario.getId()).get();
-				return "";
-	
+				model.addAttribute("usuario", usuarioComum);
+				return "redirect:painel";
+
+			case USUARIO_PJ:
+				UsuarioEntidade usuarioEntidade = new UsuarioEntidade();
+				usuarioEntidade = ues.findById(usuario.getId()).get();
+				model.addAttribute("usuario", usuarioEntidade);
+				return "redirect:painel";
+
 			default:
 				return "";
-			}			
-			
-		}
-		else {
-			return "redirect: /usuario/login";
-		}
+			}
 
+		} else {
+			redirectAttributes.addFlashAttribute("redirectAttributesLogin", "incorreto");
+			return "redirect:login";
+		}
 	}
 
 	@GetMapping("/usuario/cadastro/pessoa-fisica")
@@ -124,6 +140,15 @@ public class UsuariosController {
 
 		model.addAttribute("usuarioEntidade", usuarioEntidade);
 		return "main/usuario/cadastro-usuario-entidade";
+	}
+
+	@GetMapping("/painel")
+	public String getPainel(HttpServletRequest request, Model model) {
+
+		// paths da requisição e de redirecionamento
+		model.addAttribute("path", request.getContextPath());
+
+		return "";
 	}
 
 }
