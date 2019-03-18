fc56188e496bd10b448a6e19e8b39299e6b2b717
Lucas Copque <copque.faculdade@gmail.com>
2019-03-18 15:30:50 -0300

    classe de configuração mvc do projeto.

--- src/main/java/br/com/advocacysaude/config/WebMvcConfig.java
+++ src/main/java/br/com/advocacysaude/config/WebMvcConfig.java
@@ -6,17 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.advocacysaude.intercepctor.AutorizadorInterceptor;
 
-
-
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer  {
- 
-   //
-   @Override
-   public void addInterceptors(InterceptorRegistry registry) {
-      // LogInterceptor apply to all URLs.
-      registry.addInterceptor(new AutorizadorInterceptor()); 
-      
-   }
- 
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	//
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// LogInterceptor apply to all URLs.
+		registry.addInterceptor(new AutorizadorInterceptor());
+
+	}
+
 }
